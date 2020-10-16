b4695976309c1156972a893e6026b8f43cbc5d62
Ramadan777 <faysol@mail.ru>
2020-10-16 18:06:54 +0300

    Новые приложения

---
+++ homework1/script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.burger'),
+    nav = document.querySelector('.nav'),
+    links = document.querySelector('.nav li');
+
+function slider() {
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-ative;');
+
+        burger.classList.toggle
+    })
+}
+
+slider();
